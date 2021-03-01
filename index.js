25c4655b7d6882eb64896ac0caa6c15c124fe4f4
long bui <blong1102@gmail.com>
2021-03-01 12:21:30 +0700

    test gitignore

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', routeHome.getHome)
-
+console.log('hello')
 
 app.listen(process.env.PORT || 3000)
