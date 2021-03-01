a6434ead90bf5905758844bd46bdff68eb4bab7e
long bui <blong1102@gmail.com>
2021-03-01 18:19:16 +0700

    fix form padding

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const routeHome = require('./controller/getPage')
+const route = require('./controller/getPage')
 const path = require('path')
 const app = express()
-
-
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', routeHome.getHome)
-app.get('/uploadcv', routeHome.upload)
+app.get('/', route.getHome)
+app.get('/uploadcv', route.upload)
 
 app.listen(process.env.PORT || 3000)
