3b431def4058bdebdff03042b155885efa0f1192
long bui <blong1102@gmail.com>
2021-03-02 16:03:00 +0700

    temp for everything

--- controller/getPage.js
+++ controller/getPage.js
@@ -1,6 +1,23 @@
-exports.getHome = (req, res) => {
-    res.render('home')
-}
-exports.upload = (req, res) => {
-    res.render('uploadcv')
-}
+exports.getall = (req, res) => {
+    let url = req.originalUrl
+    switch (url) {
+        case '/':
+            res.render('home')
+            break;
+        case '/story':
+            res.render('mystory', { students: [{ name: 'long', year: '2021-2022' }] })
+            break;
+        case '/support':
+            res.render('support')
+            break;
+        case '/uploadcv':
+            res.render('uploadcv')
+            break;
+        case '/about':
+            res.render('about')
+            break;
+        default:
+            res.render('404')
+            break;
+    }
+};
