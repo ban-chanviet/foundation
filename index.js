c47b2603ca9aadefb7bc40302f332e5ec3bc4333
long bui <blong1102@gmail.com>
2021-03-31 15:27:36 +0700

    clean and restructure folder

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express')
-const route = require('./controller/getPage')
 const path = require('path')
 const app = express()
 
 
 
 const googleDriveUploader = require('./controller/driveUploader')
-const fileUpload = require('express-fileupload');
-
-const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
 
 
-app.use(fileUpload());
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(fileUpload())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('*', route.getall)
-app.post('/uploadcv', urlencodedParser, googleDriveUploader.googleDrive)
+app.get('*', require(path.join(__dirname + '/routes/getPage')))
+app.post('/uploadcv', googleDriveUploader.googleDrive)
 
 
 app.listen(process.env.PORT || 3000)
---
+++ routes/getPage.js
@@ -0,0 +1,29 @@
+module.exports = (req, res) => {
+    let url = req.originalUrl
+    switch (url) {
+        case '/':
+            res.render('home')
+            break;
+        case '/chuyencu':
+            res.render('chuyencu', { data: { students: [{ name: 'long bui bao', home: 'gia lai' }, { name: 'bui bao long', home: 'gia lai' }, { name: 'long bao bui', home: 'gia lai' }], year: '2021-2022' } })
+            break;
+        case '/gopsuc':
+            res.render('support')
+            break;
+        case '/ungtuyen':
+            res.render('uploadcv')
+            break;
+        case '/uploadcval':
+            res.render('uploadcval')
+            break
+        case '/gioithieu':
+            res.render('gioithieu')
+            break;
+        case '/thankyou':
+            res.render('thankyou')
+            break;
+        default:
+            res.render('404')
+            break;
+    }
+};
